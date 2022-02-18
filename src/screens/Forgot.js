c05ae52862cef079b3081d45050bf559c7ad3af1
Hothidani <ni.ho22@student.passerellesnumeriques.org>
2022-02-18 11:51:06 +0700

    Signup UI

--- src/screens/Forgot.js
+++ src/screens/Forgot.js
@@ -1,20 +1,28 @@
-import React, { Component } from 'react';
 import  TextField   from "@mui/material/TextField";
 import Button from '@mui/material/Button';
 
-const styles = {
-    palette: {
-        primary: {
-            main: '#bed000',
-        },
-        secondary: {
-            main: '#110b36',
-        },
-        error: {
-            main: '#B33A3A',
-        },
-    },
+const Forgot =()=>  {
+        return (
+            <div   style={styles.paperContainer}>
+                <div   style={styles.loginForm}>
+                    <div style={styles.loginFormLogin}>
+                        <div style={styles.loginFormHeader}>
+                            <img style={styles.loginFormLogo} alt='login-logo' src='./images/bg-login.png' />
+                            <h2>QR SCANNER</h2>
+                        </div>  
+                        <h3>Quên mật khẩu</h3>    
+                        <TextField required fullWidth   type="email" variant="standard" label="Nhập email" />
+                        <div style={styles.LoginButton} >
+                            <Button   style={styles.Button}  href="# "> Quên mật khẩu </Button>
+                        </div>
+                    </div>
+                </div>  
+                </div> 
+        );
+}
 
+
+const styles = {
     paperContainer: {
         display:'flex',
         alignItems: 'center',
@@ -27,11 +35,11 @@ const styles = {
     },
 
     loginForm: {
-        width:'400px',
+        width:'450px',
         minHeight:'400px',
         backgroundColor:'#FFFFFF',
         marginLeft:'auto',
-        marginRight:'100px',        
+        marginRight:'15%',        
         borderRadius:'16px',
         boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
     },
@@ -75,33 +83,6 @@ const styles = {
         justifyContent:'center',
         alignItems: 'center',
     },
-    
-    LinkBtn :{
-        color:'#ECA64E',
-    },
 };
-class Forgot extends Component {
-    
-    render() {
-        return (
-            <div   style={styles.paperContainer}>
-                <div   style={styles.loginForm}>
-                    <div style={styles.loginFormLogin}>
-                        <div style={styles.loginFormHeader}>
-                            <img style={styles.loginFormLogo} alt='login-logo' src='./images/bg-login.png' />
-                            <h2>QR SCANNER</h2>
-                        </div>  
-                        <h3>Đăng nhập</h3>    
-                        {/* <TextField color={touched.Password && errors.Password ? "primary" : "secondary"} required  fullWidth  id="standard-basic" label="Số điện thoại" type="number" variant="standard" /> */}
-                        <TextField required fullWidth   type="number" variant="standard" label="Nhập số điện thoại" />
-                        <div style={styles.LoginButton} >
-                            <Button   style={styles.Button}  href="# "> Quên mật khẩu </Button>
-                        </div>
-                    </div>
-                </div>  
-                </div> 
-        );
-    }
-}
 
 export default Forgot;
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,58 +1,43 @@
-import React, { Component } from 'react';
 import  TextField   from "@mui/material/TextField";
 import Button from '@mui/material/Button';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
+ 
 
-class Login extends Component {
-    
-    render() {
+const Login   =()=> {         
+     
         return (
             <div   style={styles.paperContainer}>
-                <div   style={styles.loginForm}>
+                <div    style={styles.loginForm}>
                     <div style={styles.loginFormLogin}>
                         <div style={styles.loginFormHeader}>
                             <img style={styles.loginFormLogo} alt='login-logo' src='./images/bg-login.png' />
                             <h2>QR SCANNER</h2>
                         </div>  
                         <h3>Đăng nhập</h3>    
-                        {/* <TextField color={touched.Password && errors.Password ? "primary" : "secondary"} required  fullWidth  id="standard-basic" label="Số điện thoại" type="number" variant="standard" /> */}
-                        <TextField required fullWidth   type="number" variant="standard" label="Số điện thoại" />
-                        <TextField required fullWidth sx={{ mt: 2}}   type="password" variant="standard" label="Mật khẩu" />
+                        <TextField style={styles.field} required fullWidth name="email"
+                         type="email" variant="standard" label="Email"   />
+                        <TextField required fullWidth sx={{ mt: 2}}   type="password" variant="standard"  label="Mật khẩu"  />
                         <div style={styles.loginFormCheck}>
                             <FormControlLabel control={<Checkbox  />} label="Ghi nhớ tôi" />
-                            <FormControlLabel sx={{color:'#ECA64E' }} control={<Link />} label="Quên mật khẩu?" >  
-                            </FormControlLabel>
+                            <a href="/forgot" style={styles.LinkBtn}>Quên mật khẩu?</a>
                         </div>
                         <div style={styles.LoginButton} >
-                            <Button   style={styles.Button}  href="# "> Đăng nhập </Button>
+                            <Button   style={styles.Button} type="submit"   > Đăng nhập </Button>
                         </div>
                         <div style={styles.Loginbottom}>
-                            <p>Bạn chưa có tài khoản?</p>
-                            <a style={styles.LinkBtn} href='#'>Đăng ký</a>
+                            <span>Bạn chưa có tài khoản?</span>
+                            <a style={styles.LinkBtn} href="/signup">Đăng ký</a>
                         </div>
                     </div>
                 </div>  
                 </div> 
         );
-    }
 };
 
 
 const styles = {
-    palette: {
-        primary: {
-            main: '#bed000',
-        },
-        secondary: {
-            main: '#110b36',
-        },
-        error: {
-            main: '#B33A3A',
-        },
-    },
-
+    
     paperContainer: {
         display:'flex',
         alignItems: 'center',
@@ -65,11 +50,11 @@ const styles = {
     },
 
     loginForm: {
-        width:'400px',
+        width:'450px',
         minHeight:'500px',
         backgroundColor:'#FFFFFF',
         marginLeft:'auto',
-        marginRight:'100px',        
+        marginRight:'15%',        
         borderRadius:'16px',
         boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
     },
@@ -105,7 +90,7 @@ const styles = {
     },
 
     Button:{
-        marginTop:'40px',
+        marginTop:'25px',
         backgroundColor:'#ECA64E',
         color:'#FFFF',
         padding: '12px 24px',
@@ -124,4 +109,4 @@ const styles = {
         color:'#ECA64E',
     },
 };
-export default Login;
+ export default Login;
